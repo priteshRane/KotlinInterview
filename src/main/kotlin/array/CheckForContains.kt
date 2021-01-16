77f32b50e08041301692ce50d3af251ac0a8d053
priteshRane <priteshrane01@gmail.com>
2021-01-16 20:31:23 +0530

    Check for number contains in Array

---
+++ src/main/kotlin/array/CheckForContains.kt
@@ -0,0 +1,25 @@
+package array
+
+private fun getNumbersArray(): IntArray {
+    print("Enter numbers (comma separated): ")
+    val numbers: IntArray = readLine()?.trim()?.split(',')?.map { it.toInt() }!!.toIntArray()
+    println("Int array is: ${numbers.contentToString()}")
+    return numbers
+}
+
+private fun getNumberToFine(): Int {
+    print("Enter number to Find: ")
+    val number = readLine()?.toInt()
+    println("Entered number is: $number")
+    return number!!.toInt()
+}
+
+fun checkForNumberContains(numbersArray: IntArray, numberToFind: Int) {
+    if (numbersArray.contains(numberToFind)) println("Number contain in Array") else println("Number does not contain in Array")
+}
+
+fun main() {
+    val numbersArray = getNumbersArray()
+    val numberToFind = getNumberToFine()
+    checkForNumberContains(numbersArray, numberToFind)
+}
